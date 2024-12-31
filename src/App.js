2ac7930a5e47e2f275823e0ebe5abf139e3e233c
pushpanjali29 <pushpanjali.0029@gamil.com>
2024-12-31 23:39:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isCorrect, setIsCorrect] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name === "user" && password === "password") {
+      setIsLoggedIn(true);
+      setIsCorrect(true);
+    } else {
+      console.log("wrong password");
+      setIsCorrect(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Login Page</h2>
+      {!isCorrect && <p>Invalid username or password</p>}
+
+      {!isLoggedIn ? (
+        <form action="submit" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              placeholder="username"
+              name="name"
+              id="username"
+              value={name}
+              required
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="text"
+              placeholder="password"
+              name="password"
+              id="password"
+              value={password}
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <p>Welcome, user</p>
+      )}
     </div>
   );
 }
